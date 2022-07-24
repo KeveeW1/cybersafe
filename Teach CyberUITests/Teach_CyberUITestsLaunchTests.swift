1ac0d9bc15b1a4c9d910f5dfa495e0d159e30d5a
Keira <1keira.kw@gmail.com>
2022-07-23 22:02:32 -0700

    Initial Commit

---
+++ Teach CyberUITests/Teach_CyberUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Teach_CyberUITestsLaunchTests.swift
+//  Teach CyberUITests
+//
+//  Created by Keira Wong on 7/23/22.
+//
+
+import XCTest
+
+class Teach_CyberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
